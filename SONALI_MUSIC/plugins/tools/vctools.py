a6aabf49fba0c3f5e368fe45e59515346a1ca9ba
𝖭ᴏᴛ 𝖢σᴅєʀ <shamansh3137@gmail.com>
2025-09-12 16:59:20 +0530

    ᴘᴜʀᴠɪ ʙᴏᴛs !!

--- SONALI_MUSIC/plugins/tools/vctools.py
+++ SONALI_MUSIC/plugins/tools/vctools.py
@@ -19,32 +19,26 @@ async def brah(_, msg):
     add_link = f"https://t.me/{app.username}?startgroup=true"
     reply_text = f"{text}"
 
-    # Inline Keyboard with a button to add the bot
     reply_markup = InlineKeyboardMarkup([
         [InlineKeyboardButton(text="๏ ᴊσɪη ᴠᴄ ๏", url=add_link)]
     ])
 
-    # Send the message with the keyboard
     await msg.reply(reply_text, reply_markup=reply_markup)
 
 
-# vc off
+
 @app.on_message(filters.video_chat_ended)
 async def brah2(_, msg: Message):
     text = "**😤 ᴠɪᴅᴇᴏ ᴄʜᴀᴛ ᴇɴᴅᴇᴅ 🙁**"
     add_link = f"https://t.me/{app.username}?startgroup=true"
     reply_text = f"{text}"
 
-    # Inline Keyboard with a button to add the bot
     reply_markup = InlineKeyboardMarkup([
         [InlineKeyboardButton(text="๏ ᴧᴅᴅ ϻє вᴧвყ ๏", url=add_link)]
     ])
 
-    # Send the message with the keyboard
     await msg.reply(reply_text, reply_markup=reply_markup)
     
-
-# invite members on vc
 @app.on_message(filters.video_chat_members_invited)
 async def brah3(app: app, message: Message):
     text = f"➠ {message.from_user.mention}\n\n**๏ ɪɴᴠɪᴛɪɴɢ ɪɴ ᴠᴄ ᴛᴏ ๏**\n\n**➠ **"
@@ -67,27 +61,6 @@ async def brah3(app: app, message: Message):
     except Exception as e:
         print(f"Error: {e}")
 
-
-####
-
-from sympy import sympify
-
-@app.on_message(filters.command("math", prefixes="/"))
-async def calculate_math(client, message):
-    if len(message.text.split(" ", 1)) < 2:
-        await message.reply("❍ ᴘʟᴇᴀsᴇ ᴘʀᴏᴠɪᴅᴇ ᴀ ᴍᴀᴛʜᴇᴍᴀᴛɪᴄᴀʟ ᴇxᴘʀᴇssɪᴏɴ ᴀғᴛᴇʀ /math.")
-        return
-
-    expression = message.text.split(" ", 1)[1]
-    try:
-        # Safely evaluate the mathematical expression
-        result = sympify(expression)
-        response = f"ᴛʜᴇ ʀᴇsᴜʟᴛ ɪs : {result}"
-    except Exception as e:
-        response = f"❍ ɪɴᴠᴀʟɪᴅ ᴇxᴘʀᴇssɪᴏɴ. ᴇʀʀᴏʀ: {str(e)}"
-    
-    await message.reply(response)
-    
 
 # ======================================================
 # ©️ 2025-26 All Rights Reserved by Purvi Bots (Im-Notcoder) 😎
