6d00f2eedb6007ae892947dea6b89eb80600c935
𝖭ᴏᴛ 𝖢σᴅєʀ <shamansh3137@gmail.com>
2025-09-15 10:40:19 +0530

    ᴘᴜʀᴠɪ ʙᴏᴛs !!

--- SONALI_MUSIC/utils/inline/help.py
+++ SONALI_MUSIC/utils/inline/help.py
@@ -45,7 +45,7 @@ def help_pannel(_, START: Union[bool, int] = None):
             ],
             [
                 InlineKeyboardButton(text=_["H_B_13"], callback_data="help_callback hb13",),
-                InlineKeyboardButton(text=_["BACK_BUTTON"], callback_data="MAIN_CP",),
+               # InlineKeyboardButton(text=_["BACK_BUTTON"], callback_data="MAIN_CP",),
                 InlineKeyboardButton(text=_["H_B_15"], callback_data="help_callback hb15",),
             ],
             mark,
